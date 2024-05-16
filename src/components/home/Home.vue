6b61ad9d5786b3117b2994262a4623915bfb8cf5
RODRIGO EMILIO FERREIRA NEVES <rodrigo_e_neves@estudante.sc.senai.br>
2024-05-15 22:02:18 -0300

    Ajuste do espaçamento entre os cards

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -17,7 +17,8 @@
       <v-col
         cols="12"
         md="4"
-        sm="6">
+        sm="6"
+        lg="3">
         <v-card
             color="#3A1C76"
             variant="outlined"
@@ -49,7 +50,8 @@
       <v-col
         cols="12"
         md="4"
-        sm="6">
+        sm="6"
+        lg="3">
         <v-card
             color="#3A1C76"
             variant="outlined"
@@ -81,7 +83,41 @@
       <v-col
         cols="12"
         md="4"
-        sm="6">
+        sm="6"
+        lg="3">
+        <v-card
+            color="#3A1C76"
+            variant="outlined"
+            class="mx-auto"
+          >
+            <v-card-item class="card-vaga">
+              <div>
+                <div class="mb-1 nome-vaga">
+                  Analista de Suporte ao Usuário Júnior
+                </div>
+                <div class="corpo-card-vaga">
+                  <div class="text-caption">
+                    <ul class="requisitos-vaga">
+                      <li><strong>Localização:</strong> Freeline Informática - Joinville, SC</li>
+                      <li><strong>Tipo de Contrato:</strong> CLT</li>
+                      <li><strong>Modalidade de Trabalho:</strong> Presencial</li>
+                      <li><strong>Remuneração: </strong> Salário compatível com a função + Benefícios</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </v-card-item>
+            <div class="my-2 container-empresa">
+              <img class="logo-vaga" src="https://picsum.photos/seed/picsum/150/150" alt="logo">
+              <span>Freeline Informática</span>
+            </div>
+          </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        md="4"
+        sm="6"
+        lg="3">
         <v-card
             color="#3A1C76"
             variant="outlined"
@@ -129,8 +165,9 @@
     background-color: #6832D2;
     border: thin solid #5300DB;
     border-radius: 10px;
-    min-height: 50%;
-    gap: 2em;
+    min-height: 48%;
+    gap: 2.5em;
+    padding: 0 5px;
 
     h1 {
       color: #fff;
@@ -151,7 +188,7 @@
 
   .corpo-card-vaga{
     padding: 10px 16px;
-    color: black;
+    color: #333;
   }
 
   .requisitos-vaga {
