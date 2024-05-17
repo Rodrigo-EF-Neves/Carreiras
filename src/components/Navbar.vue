c0db19a0861e9e9b023ae01f7ccbfef640da098a
Rodrigo-EF-Neves <rodrigo.emilio2004@gmail.com>
2024-05-16 21:39:53 -0300

    Movendo código de NavbarLogado para Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@
                 <div class="d-flex justify-center align-center">
                     <img class="carreiras-logo" src="../assets/Logo.png" alt="">
                 </div>
-                <!-- TODO Validar quando o usuário estiver logado (vai ser feito futuranente) -->
+                <!-- TODO Validar quando o usuário estiver logado (vai ser feito futurane) -->
                 <!-- <div class="sign-in-buttons">
                     <v-btn class="adm-btn" variant="outlined">
                         Adm
@@ -15,15 +15,70 @@
                     </v-btn>
                 </div> -->
                 <div class="user-account-avatar">
-                    <NavbarLogado/>
+                    <v-menu min-width="200px" rounded>
+                        <template v-slot:activator="{ props }">
+                            <v-btn icon v-bind="props">
+                                <v-avatar color="brown" size="large">
+                                    <span class="text-h5">{{ user.initials }}</span>
+                                </v-avatar>
+                            </v-btn>
+                        </template>
+                        <v-card>
+                            <v-card-text>
+                                <div class="mx-auto text-center">
+                                    <v-avatar color="brown">
+                                        <span class="text-h5">{{ user.initials }}</span>
+                                    </v-avatar>
+                                    <h3>{{ user.fullName }}</h3>
+                                    <p class="text-caption mt-1">
+                                        {{ user.email }}
+                                    </p>
+                                    <v-divider class="my-2"></v-divider>
+                                    <v-btn variant="text" rounded>
+                                        Home
+                                    </v-btn>
+                                    <v-divider class="my-2"></v-divider>
+                                    <v-btn variant="text" rounded>
+                                        Agenda
+                                    </v-btn>
+                                    <v-divider class="my-2"></v-divider>
+                                    <v-btn variant="text" rounded>
+                                        Mensagens
+                                    </v-btn>
+                                    <v-divider class="my-2"></v-divider>
+                                    <v-btn variant="text" rounded>
+                                        Currículo
+                                    </v-btn>
+                                    <v-divider class="my-2"></v-divider>
+                                    <v-btn variant="text" rounded>
+                                        Editar perfil
+                                    </v-btn>
+                                    <v-divider class="mt-2 mb-4"></v-divider>
+                                    <v-btn variant="text" rounded prepend-icon="mdi-logout">
+                                        Sair
+                                    </v-btn>
+                                </div>
+                            </v-card-text>
+                        </v-card>
+                    </v-menu>
                 </div>
             </div>
         </v-app-bar>
     </v-container>
 </template>
 
-<script setup>
+<script>
 import NavbarLogado from './NavbarLogado.vue';
+
+export default {
+    data: () => ({
+      user: {
+        initials: 'JD',
+        fullName: 'Junior Dev',
+        email: 'juniordev@gmail.com',
+      },
+    }),
+}
 </script>
 
 <style lang="scss">
