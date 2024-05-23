c8f3a5146d905a09c67d39712afbac281cbc43c9
Paula <paularossini96@gmail.com>
2024-05-23 15:52:33 -0300

    Alteraçao responsividade em TipoCadastro

--- src/components/tipoCadastro/TipoCadastro.vue
+++ src/components/tipoCadastro/TipoCadastro.vue
@@ -1,94 +1,79 @@
 <template>
-  <div class="content-full">
+  <v-container fluid class="bg-white fill-height d-flex flex-column first-container">
+    <v-col class="align-start" style="padding: 0px!important;">
+      <v-row style="display:flex;flex-direction: row;" class="bg-white">
 
-    <div class="div-logo">
-      <img class="carreiras-logo" src="../../assets/logo.png" alt="" />
-    </div>
+        <v-col style="display:flex;flex-direction: column;align-items: center; width:100%;padding: 0px !important"
+          cols="12" md="6">
+          <div class="div-logo">
+            <img class="carreiras-logo" src="../../assets/logo.png" alt="Logo" />
+          </div>
+          <h1 style="color: #4A148C; margin-top: 50px;">Candidato</h1>
+          <v-btn style="font-weight: bold; color: white;" dark
+            class="v-btn--size-x-large bg-purple-darken-4">Entrar</v-btn>
+          <img class="img-person" src="../../assets/happy-man.png" alt="Happy Man" />
+        </v-col>
 
-    <div class="candidato-empresa">
-      <div class="content-candidato">
-        <div class="center-info">
-          <h1 class="titulo">Cadidato</h1>
-          <v-btn class="bg-purple-darken-4">
-            Entrar
-          </v-btn>
-          <img class="img-person" src="../../assets/happy-man.png" alt="" />
-        </div>
-      </div>
-
-      <div class="content-empresa">
-        <div class="center-info">
-          <h1 class="titulo" style="color: white !important;">Empresa</h1>
-          <v-btn class="bg-purple-lighten-5" variant="outlined">
-            Entrar
-          </v-btn>
-          <img class="img-people" src="../../assets/businesspeople.png" alt="" />
-        </div>
-      </div>
-    </div>
-
-  </div>
+        <v-col style="display:flex;flex-direction: column;align-items: center; width:100%; padding: 0px !important;"
+          class="bg-purple-darken-4" cols="12" md="6">
+          <h1 class="titulo-empresa">Empresa</h1>
+          <v-btn style="color:rgba(58, 28, 118, 1);font-weight: bold;" outlined
+            class="v-btn--size-x-large">Entrar</v-btn>
+          <img class="img-people" src="../../assets/businesspeople.png" alt="Business People" />
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-container>
 </template>
 
-<script setup>
-//
+
+<script>
 </script>
 
-<style lang="scss" scoped>
-.titulo {
-  font-size: 25px;
-  margin-bottom: 10px;
-  color: rgba(58, 28, 118, 1);
-}
-.candidato-empresa {
-  display: flex;
-  flex-direction: row;
-  width: 100%;
-}
-.content-full {
-  display: flex;
-  flex-direction: column;
-  position: fixed;
-  width: 100%;
-  background-color: rgba(58, 28, 118, 1);
-}
+<style scoped>
 .div-logo {
   background-color: rgb(255, 255, 255);
-  margin-right: 50%;
+  width: 100%;
+  padding: 20px;
 }
 .carreiras-logo {
-  width: 22%;
+  width: 230px;
   height: auto;
-  margin: 20px 20px;
 }
-.content-candidato {
-  width: 50%;
-  background-color: rgb(255, 255, 255);
+.titulo-empresa {
+  color: white;
+  margin-top: 195px;
 }
-
-.content-empresa {
-  width: 50%;
-}
-
-.adm-btn {
-    border: thin solid #3A1C76 !important;
-    color: #3A1C76 !important;
-}
-
-.center-info {
-  margin-top: 100px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
 .img-people {
   width: 100%;
-  margin-top: 60px;
+  margin-top: 100px;
 }
-
 .img-person {
-  width: 40%;
-  margin-top: 120px;
+  width: 50%;
+  margin-top: 100px;
+}
+.first-container{
+  width: 100%; 
+  position: fixed;
+}
+@media (max-width: 955px) {
+  .titulo-empresa {
+    margin-top: 80px;
+  }
+
+  .img-people,
+  .img-person {
+    margin-top: 20px;
+  }
+
+  .div-logo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .first-container {
+    position: static; 
+  }
 }
 </style>
