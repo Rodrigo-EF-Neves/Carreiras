8931d79eb4f5141d5855584b954d6e994549012b
RODRIGO EMILIO FERREIRA NEVES <rodrigo_e_neves@estudante.sc.senai.br>
2024-05-29 21:42:21 -0300

    Continunando criação da tela da empresa

--- src/components/perfil/empresa/PerfilEmpresa.vue
+++ src/components/perfil/empresa/PerfilEmpresa.vue
@@ -3,12 +3,18 @@
     <v-container fluid="true">
         <div class="empresa-info">
             <div class="capa"></div>
-            <div class="pos-capa">
-
+            <div class="pos-capa">            
+                <div class="empresa-descricao">
+                    <img class="perfil-foto" src="https://picsum.photos/seed/picsum/150/150" alt="Foto de perfil">
+                    <div>
+                        <h1>Nome fantasia</h1>
+                        <span>Localização</span>
+                    </div>
+                </div>
             </div>
-            <div class="empresa-imagem">
-
-            </div>
+                <div class="empresa-texto">
+                    <p>Em um circo cheio de diversão, lhamas fofas estão super felizes! Com seus tutus coloridos e sorrisos brilhantes, elas começam a dançar balé. Saltitam de um lado para o outro, fazendo piruetas engraçadas e rodopiando com a maior alegria. O público ri e aplaude enquanto as lhamas deslizam pelo picadeiro, mostrando que até mesmo os animais podem ser estrelas da dança! lalalalala.</p>
+                </div>
         </div>
         <div>
             <CardVaga/>
@@ -21,12 +27,16 @@
 </script>
 
 <style lang="scss" scoped>
+// *{
+//     border: 1px solid red;
+// }
 .empresa-info {
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
     padding: 20px;
+    position: relative;
 }
 
 .capa {
@@ -46,5 +56,58 @@
     width: 100%;
     border-bottom-left-radius: 5px;
     border-bottom-right-radius: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 10px;
+    gap: 10px;
+}
+
+.empresa-descricao {
+    display: flex;
+    align-items: center;
+    width: 45%;
+    gap: 20px;
+    padding: 0 20px;
+}
+
+.empresa-texto {
+    width: 60%;
+}
+
+.perfil-foto {
+    border: 2px solid #3A1C76;
+    border-radius: 50%;
+}
+
+@media (max-width: 800px) {
+    .empresa-info {
+        padding: 0;
+        margin-bottom: 30px;
+        position: relative;
+    }
+
+    .pos-capa {
+        flex-direction: column;
+        height: fit-content;
+        padding: 10px 5px;
+    }
+
+    .empresa-descricao {
+        flex-direction: column;
+        text-align: center;
+        width: fit-content;
+        gap: 10px;
+        padding: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .empresa-texto {
+        width: 90%;
+        text-align: justify;
+    }
 }
 </style>
