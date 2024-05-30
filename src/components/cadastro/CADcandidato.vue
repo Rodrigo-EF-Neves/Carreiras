d68fb3b09f0decbc246e3d3e8f616a327521ed93
FELIPE ADRIANO WISBECK <felipe_wisbeck@estudante.sc.senai.br>
2024-05-29 21:52:31 -0300

    Criada base para telas de cadastro

---
+++ src/components/cadastro/CADcandidato.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-container fluid="true" class="login-container fill-height">
+      <v-row style="margin: 0;" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <div class="logo-container text-center">
+            <img class="carreiras-logo" src="/src/assets/Logo_branco.png" alt="">
+          </div>
+          <v-card class="elevation-12" rounded="xl" >
+            <v-card-text>
+              <h1 class="text-center my-1">Login</h1>
+              <v-form class="my-3"  @submit.prevent>
+                <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  label="E-mail"
+                  class="mb-4"
+                  bg-color="#F7F7F7"
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  :rules="passwordRules"
+                  label="Senha"
+                  class="mb-4"
+                  bg-color="#F7F7F7"
+                ></v-text-field>
+                <div class="sign-in-buttons d-flex justify-center">
+                  <v-btn lass="adm-btn" class="bg-purple-darken-4 me-2">Entrar</v-btn>
+                  <v-btn class="adm-btn " variant="outlined">Cadastre-se</v-btn>
+                </div>
+                <div class="api-google">
+                  <br>
+                  <!--Usar aqui a api-->
+                  <br>
+                </div>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      password: '',
+      passwordRules: [(v) => !!v || 'Password is required'],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+  background-color: #6832D2 !important;
+  height: 100% !important;
+}
+
+.v-row {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 75vh;
+}
+
+.logo-container {
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+ 
+.carreiras-logo {
+  height: 70px;
+  width: fit-content;
+  margin: 0 10px;
+}
+
+.sign-in-buttons {
+  margin-top: 20px;
+}
+
+.adm-btn {
+  border: thin solid #3a1c76!important;
+  color: #3a1c76!important;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Index from '@/pages/index.vue'; // Importa o componente Index
 import Chat from '@/pages/chat.vue';
 import Login from '@/components/login/Login.vue';
+import CADempresa from '@/components/cadastro/CADempresa';
+import CADcandidato from '@/components/cadastro/CADcandidato';
 import EntrarEmpresaOuCandidato from '@/components/EntrarEmpresaOuCandidato.vue';
 
 const router = createRouter({
@@ -10,6 +12,8 @@ const router = createRouter({
     { path: '/', component: Index }, // Página Index como a rota padrão
     { path: '/chat', component: Chat},
     { path: '/login', component: Login},
+    { path: '/cadastro', component: CADempresa},
+    { path: '/cadastro', component: CADcandidato},
     { path: '/empresa_candidato', component: EntrarEmpresaOuCandidato}
   ],
 });
