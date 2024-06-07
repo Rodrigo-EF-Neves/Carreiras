1aea9cb9f78b648fe4f95acd82c86f0dda833b35
Rodrigo-EF-Neves <rodrigo.emilio2004@gmail.com>
2024-06-06 22:02:35 -0300

    Feita a responsividade da tela de cadastro de empresa

--- src/components/cadastro/CADempresa.vue
+++ src/components/cadastro/CADempresa.vue
@@ -8,198 +8,194 @@
         <v-card class="px-4 elevation-12 signup-card" rounded="xl">
           <v-card-text>
             <h1 class="text-center my-1">Cadastro</h1>
-            <v-form class="my-3" @submit.prevent>
+            <v-form class="my-4" @submit.prevent>
               <v-row>
-                <v-col cols="6">
+                <v-col cols="12" sm="6" md="6" lg="6">
                   <v-text-field
                     v-model="razaoSocial"
                     :rules="razaoSocialRules"
                     label="Razão social"
-                    class="mb-4"
                     bg-color="#F7F7F7"
+                    density="compact"
                   ></v-text-field>
                 </v-col>
-                <v-col cols="6">
+                <v-col cols="12" sm="6" md="6" lg="6">
                   <v-text-field
                     v-model="nomeFantasia"
                     :rules="nomeFantasiaRules"
                     label="Nome fantasia"
-                    class="mb-4"
                     bg-color="#F7F7F7"
+                    density="compact"
                   ></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
-                <v-col cols="6">
-                  <v-text-field
+                <v-col cols="12" sm="6" md="6" lg="6">
+                    <v-text-field
                       v-model="email"
                       :rules="emailRules"
                       label="Email"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                 </v-col>
-                <v-col cols="3">
-                  <v-text-field
+                <v-col cols="12" sm="3" md="3" lg="3">
+                    <v-text-field
                       v-model="telefone"
                       :rules="telefoneRules"
                       label="Telefone"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="3">
+                  <v-col cols="12" sm="3" md="3" lg="3">
                     <v-text-field
                       v-model="celular"
                       :rules="celularRules"
                       label="Celular"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                   </v-col>
               </v-row>
               <v-row>
-                <v-col cols="3">
+                <v-col cols="12" sm="3" md="3" lg="3">
                    <v-text-field
                       v-model="cnpj"
                       :rules="cnpjRules"
                       label="CNPJ"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                    ></v-text-field>
                 </v-col>
-                <v-col cols="3">
-                  <v-text-field
+                <v-col cols="12" sm="3" md="3" lg="3">
+                   <v-text-field
                       v-model="inscricaoEstadual"
                       :rules="inscricaoEstadualRules"
                       label="Inscrição estadual"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                 </v-col>
-                <v-col cols="4">
-                  <v-text-field
+                <v-col cols="12" sm="4" md="4" lg="4">
+                   <v-text-field
                       v-model="cep"
                       :rules="cepRules"
                       label="CEP"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                 </v-col>
-                <v-col cols="2">
-                  <v-text-field
+                <v-col cols="12" sm="2" md="2" lg="2">
+                   <v-text-field
                       v-model="numero"
                       :rules="numeroRules"
                       label="Nº"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
-                <v-col cols="6">
-                  <v-text-field
+                <v-col cols="12" sm="6" md="6" lg="6">
+                   <v-text-field
                       v-model="complemento"
                       :rules="complementoRules"
                       label="Complemento"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                 </v-col>
                 <v-col>
-                  <v-text-field
+                   <v-text-field
                       v-model="endereco"
                       :rules="enderecoRules"
                       label="Endereço"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                 </v-col>
               </v-row>
               <v-row>
-                <v-col cols="3">
-                  <v-text-field
+                <v-col cols="12" sm="3" md="3" lg="3">
+                   <v-text-field
                       v-model="bairro"
                       :rules="bairroRules"
                       label="Bairro"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                 </v-col>
-                <v-col cols="3">
-                  <v-text-field
+                <v-col cols="12" sm="3" md="3" lg="3">
+                   <v-text-field
                       v-model="cidade"
                       :rules="cidadeRules"
                       label="Cidade"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                 </v-col>
-                <v-col cols="6">
-                  <v-select
-                      v-model="estado"
-                      :rules="estadoRules"
-                      :items="['Amazonas', 'Amapá', 'Santa Catarina', 'Paraná', 'Rio Grande do Sul', 'São Paulo']"
-                      label="Estado"
-                      class="mb-4"
-                      bg-color="#F7F7F7"
-                    ></v-select>
+                <v-col cols="12" sm="6" md="6" lg="6">
+                   <v-select
+                    v-model="estado"
+                    :rules="estadoRules"
+                    :items="items"
+                    label="Estado"
+                    bg-color="#F7F7F7"
+                    density="compact"
+                  ></v-select>
                 </v-col>
-                <v-col cols="6">
-                  <v-text-field
+                <v-col cols="12" sm="6" md="6" lg="6">
+                    <v-text-field
                       v-model="responsavelLegal"
                       :rules="responsavelLegalRules"
                       label="Responsável legal"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                 </v-col>
-                <v-col cols="6">
-                  <v-text-field
+                <v-col cols="12" sm="6" md="6" lg="6">
+                    <v-text-field
                       v-model="responsavelAdm"
                       :rules="responsavelAdmRules"
                       label="Responsável administrativo (RA)"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                 </v-col>
                 <v-row>
-                  <v-col cols="6">
-                    <v-text-field
+                  <v-col cols="12" sm="6" md="6" lg="6">
+                      <v-text-field
                         v-model="contatoRA"
                         :rules="contatoRARules"
                         label="Contato RA"
-                        class="mb-4"
+
                         bg-color="#F7F7F7"
+                        density="compact"
                       ></v-text-field>
                   </v-col>
-                  <v-col cols="3">
+                  <v-col cols="12" sm="3" md="3" lg="3">
                     <v-text-field
                       v-model="senha"
                       :rules="senhaRules"
                       label="Senha"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="3">
+                  <v-col cols="12" sm="3" md="3" lg="3">
                     <v-text-field
                       v-model="repSenha"
                       :rules="repSenhaRules"
                       label="Repetir senha"
-                      class="mb-4"
                       bg-color="#F7F7F7"
+                      density="compact"
                     ></v-text-field>
                   </v-col>
                 </v-row>
               </v-row>
-              <div class="sign-in-buttons d-flex justify-center mt-4">
-                <v-btn class="adm-button bg-purple-darken-4" size="large">Salvar</v-btn>
-              </div>
-              <div class="api-google">
-                <br>
-                <!--Usar aqui a api-->
-                <br>
+              <div class="sign-in-buttons d-flex justify-center my-4">
+                <v-btn class="adm-button bg-purple-darken-4 mt-4 mb-2" size="large">Salvar</v-btn>
               </div>
             </v-form>
           </v-card-text>
@@ -214,15 +210,36 @@ export default {
   data() {
     return {
       razaoSocial: '',
-      razaoSocialRules: [
-        (v) => !!v || 'A razão social é obrigatória',
-      ],
       nomeFantasia: '',
-      nomeFantasiaRules: [
-        (v) => !!v || 'O nome fantasia é obrigatório',
-      ],
-      password: '',
-      passwordRules: [(v) => !!v || 'A senha é obrigatória'],
+      email: '',
+      telefone: '',
+      celular: '',
+      cnpj: '',
+      inscricaoEstadual: '',
+      cep: '',
+      numero: '',
+      complemento: '',
+      endereco: '',
+      bairro: '',
+      cidade: '',
+      estado: '',
+      responsavelLegal: '',
+      responsavelAdm: '',
+      contatoRA: '',
+      senha: '',
+      repSenha: '',
+      razaoSocialRules: [(v) => !!v || 'A razão social é obrigatória'],
+      nomeFantasiaRules: [(v) => !!v || 'O nome fantasia é obrigatório'],
+      estadoRules: [(v) => !!v || 'O estado é obrigatório'],
+      senhaRules: [(v) => !!v || 'A senha é obrigatória'],
+      items: [
+        'Amazonas', 
+        'Amapá',
+        'Santa Catarina', 
+        'Paraná', 
+        'Rio Grande do Sul', 
+        'São Paulo'
+      ]
     };
   },
 };
@@ -254,8 +271,8 @@ export default {
 }
 
 .carreiras-logo {
-  height: 90px;
-  width: 300px;
+  height: auto;
+  width: 210px;
   margin: 0 10px;
 }
 
