4fb407da317c066c7d794bbf1049a170906bc969
FELIPE ADRIANO WISBECK <felipe_wisbeck@estudante.sc.senai.br>
2024-06-11 21:24:56 -0300

    Criada a area de a Branch para Admins

--- src/components/admin/CADadmin.vue
+++ src/components/admin/CADadmin.vue
@@ -1,4 +1,99 @@
 <template>
+  <v-container fluid class="login-container fill-height">
+      <v-row style="margin: 0;" justify="center">
+        <v-col cols="12" sm="8" md="6">
+          <div class="logo-container text-center">
+            <img class="carreiras-logo" src="/src/assets/Logo_branco.png" alt="">
+          </div>
+          <v-card class="elevation-12" rounded="xl" >
+            <v-card-text>
+              <h1 class="text-center my-1">Login</h1>
+              <v-form class="my-3"  @submit.prevent>
+                <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  label="E-mail"
+                  class="mb-4"
+                  bg-color="#F7F7F7"
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  :rules="passwordRules"
+                  label="Senha"
+                  class="mb-4"
+                  bg-color="#F7F7F7"
+                ></v-text-field>
+                <div class="sign-in-buttons d-flex justify-center">
+                  <v-btn lass="adm-btn" class="bg-purple-darken-4 me-2">Entrar</v-btn>
+                  <v-btn class="adm-btn " variant="outlined">Cadastre-se</v-btn>
+                </div>
+                <div class="api-google">
+                  <br>
+                  <!--Usar aqui a api-->
+                  <br>
+                </div>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      password: '',
+      passwordRules: [(v) => !!v || 'Password is required'],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login-container {
+  background-color: #6832D2 !important;
+  height: 100% !important;
+}
+
+.v-row {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 75vh;
+}
+
+.logo-container {
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+ 
+.carreiras-logo {
+  height: 70px;
+  width: fit-content;
+  margin: 0 10px;
+}
+
+.sign-in-buttons {
+  margin-top: 20px;
+}
+
+.adm-btn {
+  border: thin solid #3a1c76!important;
+  color: #3a1c76!important;
+}
+</style>
+
+
+<!--<template>
   <v-container fluid class="signup-container fill-height">
     <v-row style="margin: 0" justify="center">
       <v-col cols="12" sm="9" md="10" lg="11">
@@ -21,7 +116,6 @@
                     label="Nome Social"
                     bg-color="#F7F7F7"
                     density="compact"
-
                   ></v-text-field>
                 </v-col>
                 <v-col cols="12" sm="6" md="6" lg="6">
@@ -158,6 +252,16 @@
                     density="compact"
                   ></v-text-field>
                 </v-col>
+                <v-col cols="12" sm="6" md="6" lg="6">
+                  <v-select
+                    v-model="permissao"
+                    :rules="permissaoRules"
+                    :items="items"
+                    label="Permissao"
+                    bg-color="#F7F7F7"
+                    density="compact"
+                  ></v-select>
+                </v-col>
               </v-row>
               <div class="sign-in-buttons d-flex justify-center my-4">
                 <v-btn lass="adm-btn" class="bg-purple-darken-4 me-2"
@@ -176,23 +280,26 @@
 export default {
   data() {
     return {
-      nameSocial:'',
-      nomeCompleto:'',
-      email:'',
-      phone:'',
-      cellphone:'',
-      cpf:'',
-      cep:"",
-      confirmcep:'',
-      numCasa:'',
-      complemento:'',
-      endereco:'',
-      bairro:'',
-      cidade:'',
-      cidade:'',
-      estado:'',
-      password:'',
-      confirmPassword:'',
+      nameSocial: "",
+      nomeCompleto: "",
+      email: "",
+      phone: "",
+      cellphone: "",
+      cpf: "",
+      cep: "",
+      confirmcep: "",
+      numCasa: "",
+      complemento: "",
+      endereco: "",
+      bairro: "",
+      cidade: "",
+      cidade: "",
+      estado: "",
+      password: "",
+      confirmPassword: "",
+      permissao: "",
+
+      items: ["Jonas", "Nego Ban", "DJ Marquinhos", "Ronaldinho Gaucho"],
 
       namesocialRules: [
         (v) => !!v || "Nome Social Requerido",
@@ -322,4 +429,5 @@ export default {
   color: #3a1c76 !important;
   size: "large";
 }
-</style>
+</style>   
+-->
